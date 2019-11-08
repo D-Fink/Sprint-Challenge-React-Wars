61ce0b4d43679bb845712e063d00e3776b87999b
D-Fink <dfink4badjuju@gmail.com>
2019-11-08 11:47:15 -0700

    mvp

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
+import styled from "styled-components";
+
+const CharacterContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin: 0 auto;
+`;
+const Characters = styled.div`
+  background-color: rgba(211,211,211,0.8);
+  width: 45%;
+  padding: 2%;
+  margin: 0.5%;
+`;
+const CharacterNames= styled.h3`
+  background-color: rgba(128,128,128,0.8);
+  padding: 1%;
+  font-size: 50px;
+`;
+const CharacterAttr= styled.p`
+  font-size: 35px;
+`;
+const Title = styled.h1`
+  font-size: 80px;
+  padding: 4%;
+`;
+const SubTitle = styled.h2`
+  font-size: 60px;
+`;
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [starChars, setStarChars] = useState([])
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(() => {
+    axios.get("https://swapi.co/api/people")
+    .then(response => {
+      console.log(response.data.results)
+      setStarChars(response.data.results)
+    }).catch(error => {
+      console.log('The force is not with you', error)
+    })
+  }, [])
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <Title className="Header">React Wars</Title>
+      <hr></hr>
+      <SubTitle>Character List</SubTitle>
+        <CharacterContainer>
+          {starChars.map((info, index) => {
+            return(
+              <Characters>
+              <CharacterNames className='name' key={index}>{info.name}</CharacterNames>
+              <CharacterAttr key={index}>Gender: {info.gender}<br/>Height: {info.height}<br/>Weight: {info.mass}<br/>Hair Color: {info.hair_color}<br/>Eye Color: {info.eye_color}</CharacterAttr>
+              </Characters>
+            )
+          })}
+        </CharacterContainer>
     </div>
   );
 }
